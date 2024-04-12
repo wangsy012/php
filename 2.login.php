f8c2fdfdec8abbdc38f41bed518c8975784880bb
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 10:14:11 +0800

    Update 2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,5 @@
 <?php 
-    echo $_POST["id"];//顯示輸入的id
+    echo $_POST["id"];//擷取用POST方式傳遞的變數顯示輸入的id
     echo "<br>";//換行
-    echo $_POST["pwd"];//顯示輸入的密碼
+    echo $_POST["pwd"];//擷取用POST方式傳遞的變數顯示輸入的密碼
 ?>
