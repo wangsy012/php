fd8d9c5e7cc46f6cf1fa03bf0dc0416d940ae5ba
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 09:49:03 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,10 +1,10 @@
 <html>
     <head><title>html+php</title></head>      
     <body>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
+        <?php echo "Hello world!"; ?> //在網頁上顯示Hello world!
+        <h1>html</h1>                 //顯示html大小是最大的h1字體
+        <?php echo "Hello world!"; ?> //在網頁上顯示Hello world!
+        <h1>html</h1>                 //顯示html大小是最大的h1字體  
+        <?php echo "Hello world!"; ?> //在網頁上顯示Hello world!
     </body>
 </html>
