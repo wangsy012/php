fe771702a331a67dc08c1abf86205582f6a981e6
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 11:18:28 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,10 +1,10 @@
 <?php
    #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");//這行代碼用於建立與資料庫的連結的變數 $conn
    #mysqli_query() 從資料庫查詢資料
-   $result=mysqli_query($conn, "select * from user");
+   $result=mysqli_query($conn, "select * from user");//創建一個查詢指令select * from user的變數$result
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-   while ($row=mysqli_fetch_array($result)) {
-     echo $row["id"]." ".$row["pwd"]."<br>";
+   while ($row=mysqli_fetch_array($result)) {//一個 while 迴圈無法抓取資料就會停止
+     echo $row["id"]." ".$row["pwd"]."<br>";//顯示immust資料庫中的id pwd
    } 
 ?>
