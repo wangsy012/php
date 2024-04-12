c7d97a8c599e9815fdbb4cb1a654b8a0b36438a9
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 11:31:09 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -7,11 +7,11 @@
    $login=FALSE;
    while ($row=mysqli_fetch_array($result)) {//一個 while 迴圈無法抓取資料就會停止
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
-       $login=TRUE;
+       $login=TRUE;//如果結果為TURE會將 $login的布林值設為TRUE
      }
    } 
    if ($login==TRUE)
-     echo "登入成功";
+     echo "登入成功";//$login的變數是TRUE會在網頁上顯示登入成功
   else
-     echo "帳號/密碼 錯誤";
+     echo "帳號/密碼 錯誤";//反之會顯示帳號/密碼 錯誤
 ?>
