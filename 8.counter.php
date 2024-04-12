58474ca537f62c35ffbcad8c2c5ee61480f3af3c
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 12:00:15 +0800

    Update 8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -1,9 +1,9 @@
 <?php
-    session_start();
-    if (!isset($_SESSION["counter"]))
-        $_SESSION["counter"]=1;
+    session_start();//開始一個session
+    if (!isset($_SESSION["counter"]))//如果session裡沒有設定counter
+        $_SESSION["counter"]=1;//將counter設定為1 
     else
-        $_SESSION["counter"]++;
+        $_SESSION["counter"]++;//反之counter+1 
 
     echo "counter=".$_SESSION["counter"];
     echo "<br><a href=9.reset_counter.php>重置counter</a>";
