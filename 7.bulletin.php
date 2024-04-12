10c4d9508ec20a7ecf411d2669f3cc74537946bf
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 11:37:05 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,7 +1,7 @@
 <?php
     error_reporting(0);
-    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-    $result=mysqli_query($conn, "select * from bulletin");
+    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");//這行代碼用於建立與資料庫的連結的變數 $conn
+    $result=mysqli_query($conn, "select * from bulletin");//創建一個查詢指令select * from user的變數$result
     echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
     while ($row=mysqli_fetch_array($result)){
         echo "<tr><td>";
