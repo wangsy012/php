369ad41184abde403cd5de5abe7fc1562fb472ad
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 10:05:00 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,6 +1,6 @@
 <?php 
     if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234"))
-        echo "登入成功";如果
+        echo "登入成功";/如果id是john pwd是john1234會顯示登入成功
     else
         echo "登入失敗";
 ?>
