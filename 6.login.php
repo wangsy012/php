81e2a08d0e887d915d913de872cbb7725dbcda14
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-12 11:24:59 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -1,8 +1,8 @@
 <?php
    #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");//這行代碼用於建立與資料庫的連結的變數 $conn
    #mysqli_query() 從資料庫查詢資料
-   $result=mysqli_query($conn, "select * from user");創建一個查詢指令select * from user的變數$result
+   $result=mysqli_query($conn, "select * from user");//創建一個查詢指令select * from user的變數$result
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
    $login=FALSE;
    while ($row=mysqli_fetch_array($result)) {
