0d7ebb8d1e021d3ebabd06da88931b112e32c85c
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-04-18 21:29:14 +0800

    Update 8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -5,6 +5,6 @@
     else
         $_SESSION["counter"]++;//反之counter+1 
 
-    echo "counter=".$_SESSION["counter"];
-    echo "<br><a href=9.reset_counter.php>重置counter</a>";
+    echo "counter=".$_SESSION["counter"];//在網頁上顯示counter= 和 counter的數值
+    echo "<br><a href=9.reset_counter.php>重置counter</a>";//創一個超連結可以進入9.reset_counter.php的網頁
 ?>
