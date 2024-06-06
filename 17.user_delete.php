5ff86179874442dcfa505574c0658de4e89ca1f0
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-06-06 23:03:01 +0800

    Update 17.user_delete.php

--- 17.user_delete.php
+++ 17.user_delete.php
@@ -1,6 +1,6 @@
 <?php
     error_reporting(0); // 關閉錯誤報告
-    session_start();// Session
+    session_start();// 開啟Session
     if (!$_SESSION["id"]) {//如Session沒id
         echo "請登入帳號";//網頁印出登入帳號
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";// 三秒後跳到網頁2.login.html
