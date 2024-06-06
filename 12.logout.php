de3fad54b60e83a871bfe5f29282b23d7eff42cf
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-06-06 23:01:28 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -2,6 +2,6 @@
     session_start();//開啟Session
     unset($_SESSION["id"]);//刪除Session
     echo "登出成功....";//網頁印出成功登出....
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";三秒後跳到2.login.html
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";//三秒後跳到2.login.html
 
 ?>
