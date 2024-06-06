a36016663c91ea2c6150ffd214450013820f960e
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-06-06 23:01:42 +0800

    Update 14.user_add_form.php

--- 14.user_add_form.php
+++ 14.user_add_form.php
@@ -3,7 +3,7 @@
     <body>
 <?php        
     error_reporting(0);//關閉錯誤報告
-    session_start();// Session
+    session_start();// 開啟Session
     if (!$_SESSION["id"]) {//如Session沒id
         echo "請登入帳號";//網頁印出請登入帳號
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";// 三秒後跳到網頁2.login.html
