2470518db92184656b33199b9c178f820a5df1ca
wangsy012 <134042455+wangsy012@users.noreply.github.com>
2024-06-06 22:46:02 +0800

    Update 14.user_add_form.php

--- 14.user_add_form.php
+++ 14.user_add_form.php
@@ -1,20 +1,20 @@
 <html>
-    <head><title>新增使用者</title></head>
+    <head><title>新增使用者</title></head> //網頁標題為新增使用者
     <body>
 <?php        
-    error_reporting(0);
-    session_start();
-    if (!$_SESSION["id"]) {
-        echo "請登入帳號";
-        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    error_reporting(0);//關閉錯誤報告
+    session_start();// Session
+    if (!$_SESSION["id"]) {//如Session沒id
+        echo "請登入帳號";//網頁印出請登入帳號
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";// 三秒後跳到網頁2.login.html
     }
     else{    
         echo "
-            <form action=15.user_add.php method=post>
-                帳號：<input type=text name=id><br>
-                密碼：<input type=text name=pwd><p></p>
-                <input type=submit value=新增> <input type=reset value=清除>
-            </form>
+            <form action=15.user_add.php method=post>//出現表單方法使用post送出後會跳到15.user_add.php
+                帳號：<input type=text name=id><br>//帳號欄名稱為id
+                密碼：<input type=text name=pwd><p></p> //密碼欄名稱為pwd
+                <input type=submit value=新增> <input type=reset value=清除>//新增和清除按鈕
+            </form>//
         ";
     }
 ?>
